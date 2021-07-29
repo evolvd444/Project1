bc8f253f8375c93b8c07b9c5c9a4da40695d8d02
omarevolving@gmail.com <Kingoz08x!>
2021-07-29 14:45:45 -0400

    c3 grabbed some stuff

---
+++ script.js
@@ -0,0 +1,8 @@
+//grabbing what i need using query selector
+let targets = document.querySelector(".bullsEye")
+let game = document.querySelector(".game")
+let start = document.querySelector(".start")
+let score = document.querySelector(".score")
+
+//okay now i want to the divs to flip/change to from one side to the other individually
+//just found how to creat flip card in w3schools https://www.w3schools.com/howto/howto_css_flip_card.asp
