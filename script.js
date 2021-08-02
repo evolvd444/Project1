7c7e12825a8917b8742e6bb08567dfcf59824d10
omarevolving@gmail.com <Kingoz08x!>
2021-08-02 13:57:26 -0400

    created for loop to get van damme to be in a random place

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ let targets = document.querySelector(".bullsEye")
 let game = document.querySelector(".game")
 let start = document.querySelector(".start")
 let score = document.querySelector(".score")
-let front = document.querySelector(".flip-card-front")
-let back = document.querySelector(".flip-card-back")
-
+let front = document.querySelector(".front")
+let back = document.querySelectorAll(".back")
+let vanDamme = document.createElement("img")
+let hostage = document.createElement("img", src="https://ak.picdn.net/shutterstock/videos/1021295671/thumb/11.jpg" )
 //okay now i want to the divs to flip/change to from one side to the other individually
 //just found how to creat flip card in w3schools https://www.w3schools.com/howto/howto_css_flip_card.asp
 // <button class= "start">Start!</button>
@@ -14,11 +15,29 @@ let back = document.querySelector(".flip-card-back")
 //then i need to register point take aways for killing hostage and health loss from not shooting vandamn in time.
 // okay to create images to go randomly i have to create element
 
-let vanDamme = document.createElement(img, src="https://i5.walmartimages.com/asr/8ed88348-fd7f-492b-9ad9-640d392fdac1_1.1bc02bb917b325271cc8593cfeda29b6.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff", alt="Avatar", style="width:300px;height:300px;" )
-document.appendChild()
 
-for (let i = 0; )
+//here i have tested to append child.. it works... now i have to get it to append to a random div.
+//i also have to make that random div flip at a certain speed2
+// function randomize(back) {
+  
+//   }
+  
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random()) * (9 - 0) + 0;
+   
+  }
+  console.log(getRandomArbitrary())
+//   randomize(back);
+  console.log(back);
+for (let i = 0; i < Math.floor(Math.random() * 9); i++) {
+    
+    vanDamme.src = "https://i5.walmartimages.com/asr/8ed88348-fd7f-492b-9ad9-640d392fdac1_1.1bc02bb917b325271cc8593cfeda29b6.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff";
+    vanDamme.style = "width:300px; height:300px";
+   
+    let test = back[i].appendChild(vanDamme)
+    console.log(test)
 
+}
 
 
 
@@ -32,7 +51,7 @@ for (let i = 0; )
 
 
 //<div class="flip-card">
-{/* <div class="flip-card-inner">
+/* <div class="flip-card-inner">
 <div class="flip-card-front">
   <h1>Kill Van Damme </h1>
   <h1>Before he Kills you! </h1>
@@ -42,4 +61,4 @@ for (let i = 0; )
 
 </div>
 </div>
-</div> */}
+</div> */
