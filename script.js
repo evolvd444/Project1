ec6cf36d7cd668f8e495378421acfca86da1898f
omarevolving@gmail.com <Kingoz08x!>
2021-08-02 10:32:45 -0400

    frame work pretty much set up starting functionality

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let targets = document.querySelector(".bullsEye")
 let game = document.querySelector(".game")
 let start = document.querySelector(".start")
 let score = document.querySelector(".score")
-
+let front = document.querySelector(".flip-card-front")
+let back = document.querySelector(".flip-card-back")
 
 //okay now i want to the divs to flip/change to from one side to the other individually
 //just found how to creat flip card in w3schools https://www.w3schools.com/howto/howto_css_flip_card.asp
@@ -11,7 +12,25 @@ let score = document.querySelector(".score")
 //okay now i have  the divs flipping on hover.. but now i need to get the divs to turn around randomly themselves
 //also how to get hostages and randomize them.
 //then i need to register point take aways for killing hostage and health loss from not shooting vandamn in time.
-// ok ay to create images to go randomly i have to create element 
+// okay to create images to go randomly i have to create element
+
+let vanDamme = document.createElement(img, src="https://i5.walmartimages.com/asr/8ed88348-fd7f-492b-9ad9-640d392fdac1_1.1bc02bb917b325271cc8593cfeda29b6.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff", alt="Avatar", style="width:300px;height:300px;" )
+document.appendChild()
+
+for (let i = 0; )
+
+
+
+
+
+
+
+
+
+
+
+
+
 //<div class="flip-card">
 {/* <div class="flip-card-inner">
 <div class="flip-card-front">
