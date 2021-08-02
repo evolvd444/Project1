13938e4b062aed3a98739607d7a852e501d2eca5
omarevolving@gmail.com <Kingoz08x!>
2021-08-02 09:45:09 -0400

    getting there

--- script.js
+++ script.js
@@ -4,5 +4,23 @@ let game = document.querySelector(".game")
 let start = document.querySelector(".start")
 let score = document.querySelector(".score")
 
+
 //okay now i want to the divs to flip/change to from one side to the other individually
 //just found how to creat flip card in w3schools https://www.w3schools.com/howto/howto_css_flip_card.asp
+// <button class= "start">Start!</button>
+//okay now i have  the divs flipping on hover.. but now i need to get the divs to turn around randomly themselves
+//also how to get hostages and randomize them.
+//then i need to register point take aways for killing hostage and health loss from not shooting vandamn in time.
+// ok ay to create images to go randomly i have to create element 
+//<div class="flip-card">
+{/* <div class="flip-card-inner">
+<div class="flip-card-front">
+  <h1>Kill Van Damme </h1>
+  <h1>Before he Kills you! </h1>
+</div>
+<div class="flip-card-back">
+  <img src="https://i5.walmartimages.com/asr/8ed88348-fd7f-492b-9ad9-640d392fdac1_1.1bc02bb917b325271cc8593cfeda29b6.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff" alt="Avatar" style="width:300px;height:300px;">
+
+</div>
+</div>
+</div> */}
