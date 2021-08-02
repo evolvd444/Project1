c6d6bf776b4146a0290f179c84a97fe08175a899
omarevolving@gmail.com <Kingoz08x!>
2021-08-02 19:01:38 -0400

    got progress on point system to make it take away points for hittigng hostages

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //grabbing what i need using query selector
-let targets = document.querySelector(".bullsEye")
-let game = document.querySelector(".game")
-let startButton = document.querySelector("#start")
-let score = document.querySelector(".score")
-let front = document.querySelector(".front")
-let back = document.querySelectorAll(".back")
-let card = document.querySelector(".card")
-let inner = document.querySelectorAll(".inner")
-let vanDamme = document.createElement("img")
-let hostage = document.createElement("img", src="https://ak.picdn.net/shutterstock/videos/1021295671/thumb/11.jpg" )
+let targets = document.querySelector(".bullsEye");
+let game = document.querySelector(".game");
+let startButton = document.querySelector("#start");
+let score = document.querySelector(".score");
+let front = document.querySelector(".front");
+let back = document.querySelectorAll(".back");
+let card = document.querySelector(".card");
+let inner = document.querySelectorAll(".inner");
+let vanDamme = document.createElement("img");
+let hostage = document.createElement("img");
 //okay now i want to the divs to flip/change to from one side to the other individually
 //just found how to creat flip card in w3schools https://www.w3schools.com/howto/howto_css_flip_card.asp
 // <button class= "start">Start!</button>
@@ -17,47 +17,63 @@ let hostage = document.createElement("img", src="https://ak.picdn.net/shuttersto
 //then i need to register point take aways for killing hostage and health loss from not shooting vandamn in time.
 // okay to create images to go randomly i have to create element
 
-
 //here i have tested to append child.. it works... now i have to get it to append to a random div.
 //i also have to make that random div flip at a certain speed2
 
-
 // so this for loop randomly itterates through the array so vandame appears in different spots every time screen refreshes
 //but i want it to autamtically switch van damme so i think i need to create a function
-function start(){
-    for (let i = 0; i < Math.floor(Math.random() * 9); i++) {
-        let j = Math.floor(Math.random() * 9)
-        inner[j].style = "transform: rotateY(180deg)";
-    }    
-    // this loop is for generating a random ammount of hostages
-    for (let i = 0; i < Math.floor(Math.random() * 9); i++) {
-        let j = Math.floor(Math.random() * 9)
-        inner[j].style = "transform: rotateY(180deg)";
-    }    
+//need to get the cards to refresh
 
-    //this is so vandamme can go into random places
-    let i = Math.floor(Math.random() * 9)
-    vanDamme.src = "https://i5.walmartimages.com/asr/8ed88348-fd7f-492b-9ad9-640d392fdac1_1.1bc02bb917b325271cc8593cfeda29b6.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff";
-    vanDamme.style = "width:300px; height:300px";
-    let test = back[i].appendChild(vanDamme)
-    inner[i].style = "transform: rotateY(180deg)";
-    console.log(test)
+// this is to activate all at the same time
+function play() {
+  let inner = document.querySelectorAll(".inner");
+  // this loop is for resetting the board
+  for (let i = 0; i < inner.length; i++) {
+    if (inner[i].dataset.activemode != "Front") {
+      inner[i].dataset.activemode = "Front";
+      inner[i].style = "transform: rotateY(0deg)";
+    }
+    //resets the point back -10
+    back[i].dataset.points = "0";
+  }
+  // this loop is for generating a random ammount of hostages
+  for (let i = 0; i < Math.floor(Math.random() * 9); i++) {
+    let j = Math.floor(Math.random() * 9);
 
+    inner[j].dataset.activemode = "back";
+    inner[j].style = "transform: rotateY(180deg)";
+    back[i].dataset.points = "-10";
+    card[i].setAttribute(
+      "onclick",
+      "calculatePoints(" + back[i].dataset.points + ")"
+    );
+  }
+
+  //this is so vandamme can go into random places
+  let i = Math.floor(Math.random() * 9);
+  vanDamme.src =
+    "https://i5.walmartimages.com/asr/8ed88348-fd7f-492b-9ad9-640d392fdac1_1.1bc02bb917b325271cc8593cfeda29b6.jpeg?odnWidth=612&odnHeight=612&odnBg=ffffff";
+  vanDamme.style = "width:300px; height:300px";
+  let test = back[i].appendChild(vanDamme);
+  inner[i].style = "transform: rotateY(180deg)";
+  //this changed activemode(located in html) after flipping the card
+  inner[i].dataset.activemode = "back";
+  back[i].dataset.points = "5";
+  card[i].setAttribute(
+    "onclick",
+    "calculatePoints(" + back[i].dataset.points + ")"
+  );
+  console.log(test);
 }
-
-
-
+//this calculate points lol
+function calculatePoints(numPoints) {
+  //did parse int to see through the string and convert it to number -10
+  points += parseInt(numPoints);
+}
+function start() {
+  let points = 0;
+}
 // startButton.addEventListener("onclick", start())
-
-
-
-
-
-
-
-
-
-
 
 //<div class="flip-card">
 /* <div class="flip-card-inner">
